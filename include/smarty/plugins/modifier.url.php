a4c8e3bb93dc35caad6dc8d9438304f5bf30fd5f
indianiso1 <indianiso1@gmail.com>
2019-11-21 00:23:23 +0200

    Making basis classes from .inc.php files

--- include/smarty/plugins/modifier.url.php
+++ include/smarty/plugins/modifier.url.php
@@ -4,17 +4,16 @@
  * Type:     modifier
  * Name:     url
  * Purpose:  transform URL to appropriate
- *
  * @author indianiso1 <indianiso1 at gmail dot com>
  *
- * @param string      $string        input string
- * @param string|null $real_path  If set will take custom link which needs to prepend index
+ * @param string      $string    input string
+ * @param string|null $php
+ * @param string|null $real_path If set will take custom link which needs to prepend index
  *
  * @return string made url
+ *
  */
-function smarty_modifier_url($string, string $real_path = NULL)
+function smarty_modifier_url(string $string, string $php = NULL, string $real_path = NULL)
 {
-    if ($real_path)
-        return Route::getBaseURL() . '/' . $real_path . '/index.php/' . $string;
-    return Route::makeURL($string);
+    return Route::makeURL($string, $php, $real_path);
 }
--- index.php
+++ index.php
@@ -1,33 +1,20 @@
 <?php
-
-/* 	
-
-	AMXBans v6.0
-	
-	Copyright 2009, 2010 by SeToY & |PJ|ShOrTy
-
-	This file is part of AMXBans.
-
-    AMXBans is free software, but it's licensed under the
-	Creative Commons - Attribution-NonCommercial-ShareAlike 2.0
-
-    AMXBans is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-    You should have received a copy of the cc-nC-SA along with AMXBans.  
-	If not, see <http://creativecommons.org/licenses/by-nc-sa/2.0/>.
-
-*/
-
+/**
+ *    AMXBans v7
+ * Copyright 2009, 2010 by SeToY & |PJ|ShOrTy, 2019 by indianiso1
+ * This file is part of AMXBans.
+ *
+ * AMXBans is free software, but it's licensed under the Creative Commons - Attribution-NonCommercial-ShareAlike 2.0
+ * AMXBans is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * You should have received a copy of the cc-nC-SA along with AMXBans.
+ * If not, see http://creativecommons.org/licenses/by-nc-sa/2.0/
+ */
 
 session_start();
+require_once "include/class.Config.inc";
+$config = new Config;
 
-//check for existing config file
-if(!file_exists("include/db.Config.inc")) {
-	//TODO: header("Location: setup.php");
-}
-
+/*
 require_once("include/config.inc.php");
 require_once("include/access.inc.php");
 
@@ -37,4 +24,4 @@ if(	$config->start_page == "" || $config->start_page == "index.php" || (!file_ex
 } else {
 	header("Location:$config->start_page");
 }
-?>
+*/
