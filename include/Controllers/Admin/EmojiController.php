37de070f88694397e3153fc1b560479c3efced66
indianiso1 <indianiso1@gmail.com>
2021-12-13 22:13:22 +0200

    Put a TODOs - Just as a reminder to do them

--- include/Controllers/Admin/EmojiController.php
+++ include/Controllers/Admin/EmojiController.php
@@ -8,14 +8,17 @@ class EmojiController extends BaseController
 {
     public function create()
     {
+        // TODO: implement create method (emoji creation in ACP web/settings)
     }
 
     public function store()
     {
+        // TODO: implement store method (emoji creation in ACP web/settings)
     }
 
     public function delete()
     {
+        // TODO: implement delete method (emoji list in ACP web/settings)
         htmlspecialchars_decode($_POST['code']);
     }
 }
